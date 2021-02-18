3cea15479907fd750bcff24c42826ae498acaa0b
juan <jotabeles.web@gmail.com>
2021-02-18 14:30:28 -0500

    El modo ofuscado

--- src/Models/generacion_medio_detalle.php
+++ src/Models/generacion_medio_detalle.php
@@ -1,24 +1,8 @@
 <?php
-
-namespace Fedatario\Models;
-
-use Illuminate\Database\Eloquent\Model;
-use DB;
-
-Trait generacion_medio_detalle
-{
-    protected $primaryKey = "gmd_id";
-    protected $table = "generacion_medio_detalle";
-
-    public function confirmar_gmd($gmd_id){
-
-        return  $res_gmd = $this->join("generacion_medio_detalle_captura as gmdc","gmdc.gmd_id","generacion_medio_detalle.gmd_id")
-            ->leftjoin("captura as cap","cap.captura_id","gmdc.captura_id")
-            ->leftjoin("documento as doc","doc.captura_id","cap.captura_id")
-            ->leftjoin("adetalle as ad","ad.adetalle_id","doc.adetalle_id")
-            ->select("generacion_medio_detalle.gmd_id","gmdc_id","adetalle_url")
-            ->where("generacion_medio_detalle.gmd_id",$gmd_id)
-            ->get();
-    }
-
-}
+/*   __________________________________________________
+    |  Obfuscated by YAK Pro - Php Obfuscator  2.0.12  |
+    |              on 2021-02-18 13:04:16              |
+    |    GitHub: https://github.com/pk-fr/yakpro-po    |
+    |__________________________________________________|
+*/
+ namespace Fedatario\Models; use Illuminate\Database\Eloquent\Model; use DB; trait generacion_medio_detalle { protected $GqAhM = "\x67\155\x64\x5f\151\144"; protected $kVRBq = "\147\145\156\x65\x72\141\x63\151\157\x6e\x5f\155\x65\144\151\157\137\144\145\x74\x61\x6c\x6c\145"; public function confirmar_gmd($InNiT) { return $f3fNG = $this->join("\147\x65\x6e\x65\x72\x61\143\151\157\156\x5f\x6d\145\144\x69\157\x5f\x64\145\x74\141\154\x6c\x65\x5f\143\x61\x70\x74\x75\162\141\40\141\163\x20\x67\155\x64\x63", "\147\x6d\x64\x63\56\x67\155\x64\137\151\x64", "\147\145\x6e\x65\x72\141\x63\x69\157\156\x5f\x6d\x65\144\x69\157\137\x64\x65\x74\141\x6c\x6c\145\x2e\147\155\144\x5f\x69\144")->leftjoin("\143\141\x70\x74\x75\x72\141\x20\x61\163\40\x63\x61\x70", "\143\141\160\56\x63\141\160\x74\165\162\141\x5f\151\x64", "\x67\155\144\x63\x2e\x63\141\160\164\165\162\141\x5f\x69\144")->leftjoin("\x64\x6f\143\x75\155\145\156\x74\157\40\141\163\40\144\x6f\143", "\144\x6f\143\56\x63\141\160\x74\x75\x72\141\x5f\x69\x64", "\143\141\160\x2e\x63\141\160\x74\165\162\x61\x5f\151\x64")->leftjoin("\141\x64\145\x74\141\x6c\x6c\x65\40\x61\163\x20\x61\x64", "\x61\x64\x2e\141\x64\145\x74\141\154\x6c\145\137\x69\x64", "\x64\x6f\x63\x2e\x61\x64\145\x74\x61\154\x6c\x65\x5f\x69\x64")->select("\x67\145\156\145\x72\141\x63\151\x6f\156\137\155\145\x64\151\x6f\137\x64\145\x74\x61\154\x6c\x65\56\x67\x6d\144\137\151\144", "\147\155\x64\x63\137\151\x64", "\141\144\145\164\x61\x6c\154\145\137\165\x72\154")->where("\x67\145\156\x65\x72\141\143\151\157\156\x5f\x6d\x65\144\x69\157\137\x64\145\164\x61\x6c\x6c\145\x2e\147\x6d\144\137\151\x64", $InNiT)->get(); } }
